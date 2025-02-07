3ac12bc76c471721e9a377f2bffa0bb28b85b74b
Bam <Bam449@gmail.com>
2025-02-07 18:10:20 +0300

    HW7

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -2,13 +2,16 @@ package ru.javawebinar.basejava.model;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 
-public class Resume implements Comparable <Resume>{
+public class Resume implements Comparable<Resume> {
+    protected final Logger log = Logger.getLogger(getClass().getName());
 
-    private String uuid;
-    private String fullName;
+    private final String uuid;
+    private final String fullName;
 
     private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
     private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
@@ -18,29 +21,26 @@ public class Resume implements Comparable <Resume>{
     }
 
     public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "UUID not must be null");
+        Objects.requireNonNull(fullName, "FullName not must be null");
         this.uuid = uuid;
         this.fullName = fullName;
     }
 
     @Override
     public String toString() {
-        return uuid;
+        return "Resume{" +
+                "uuid='" + uuid + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
     }
 
     public String getUuid() {
         return uuid;
     }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
     public String getFullName() {
         return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     @Override
@@ -60,6 +60,7 @@ public class Resume implements Comparable <Resume>{
 
     @Override
     public int compareTo(Resume o) {
-        return this.uuid.compareTo(o.uuid);
+        int cmp = fullName.compareTo(o.fullName);
+        return cmp != 0 ? cmp: uuid.compareTo(o.uuid);
     }
 }
--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,8 +6,11 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage <SK> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
 
     protected abstract SK getSearchKey(String uuid);
 
@@ -25,24 +28,28 @@ public abstract class AbstractStorage <SK> implements Storage {
 
 
     public void save(Resume r) {
+        LOG.info("Save " + r);
         SK searchKey = getSearchKey(r.getUuid());
         if (isExist(searchKey)) throw new ExistStorageException(r.getUuid());
         doSave(r, searchKey);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) throw new NotExistStorageException(uuid);
         return doGet(searchKey);
     }
 
     public void update(Resume r) {
+        LOG.info("Update " + r);
         SK searchKey = getSearchKey(r.getUuid());
         if (!isExist(searchKey)) throw new NotExistStorageException(r.getUuid());
         doUpdate(r, searchKey);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) throw new NotExistStorageException(uuid);
         doDelete(searchKey);
@@ -50,6 +57,7 @@ public abstract class AbstractStorage <SK> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = doCopyAll();
         Collections.sort(list);
         return list;
