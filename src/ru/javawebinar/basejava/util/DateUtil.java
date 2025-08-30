679bf9b1fae2b1577ce8907843acb73f30549685
bam <Bam449@gmail.com>
2025-08-30 20:35:12 +0300

    HW9

--- src/ru/javawebinar/basejava/util/DateUtil.java
+++ src/ru/javawebinar/basejava/util/DateUtil.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 
 public class DateUtil {
 
+public static final LocalDate NOW = LocalDate.of(3000, 1, 1);
+
+    public static LocalDate of (int startDate, int startMonth) {
         return LocalDate.of(startDate, startMonth, 1);
     }
 }
